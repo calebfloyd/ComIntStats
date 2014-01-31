3c8314eb9b39481c78b4f0ebdc94137d7db9303e
Caleb Floyd <c.floyd999@gmail.com>
2014-01-31 15:14:40 -0600

    Fixed out-of-range issues. Now throws warnings similar to other functions.

--- dpareto.R
+++ dpareto.R
@@ -1,5 +1,17 @@
-dpareto <- function(x, alpha, beta = 1)
+dpareto <- function(x, alpha, beta = 1, log = FALSE)
 {
-  result = (beta * (alpha^beta))/(x^(beta + 1))
-  return(result)
+  
+  alpha <- ifelse(alpha < x, 
+                  ifelse(alpha > 0, alpha, NaN), 0)
+    if(any(is.na(alpha)))warning("NaNs produced")
+
+  beta <- ifelse(beta > 0, beta, NaN)
+    if(any(is.na(beta)))warning("NaNs produced")
+  
+  if(log == TRUE){
+    return(log((beta * (alpha^beta))/(x^(beta + 1))))
+  }
+
+  (beta * (alpha^beta))/(x^(beta + 1))
+  
 }
